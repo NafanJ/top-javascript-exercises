33a0aea93ff0f9e0d5ea7d1f8f4bf350b70b2459
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-10-24 11:31:42 +0100

    JavaScript Tests Practice

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    // TC#3 Works with larger number first
+    if (min > max){
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    // TC#4 Negative numbers
+    if (min < 0 || max < 0)
+        return "ERROR"
+    
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
 
+    let sum = 0;
+    for (let i = min; i<=max; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
