fa5e2796d0080b2671c62ad396d617754dd3b63e
Nathan Joel <nathanjoel1@hotmail.co.uk>
2023-11-16 14:02:33 +0000

    Exercise 10 Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    const num = parseInt(fib);
 
+    if (num < 0) return "OOPS"
+
+    let n1 = 0, n2 = 1, nextTerm;
+
+    for (let i = 1; i <= num; i++) {
+        console.log(n1);
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    return n1;
 };
 
 // Do not edit below this line
